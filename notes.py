12173b0bbbfd32df5dd5abcfbab3e5604ade606a
aidenfernandes <aidenfernandes7@gmail.com>
2026-08-10 09:40:49 +0530

    Added styling and delete function

--- notes.py
+++ notes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 from flask_login import current_user, login_required
 from aidenflask import db
 from models import Note
@@ -16,6 +16,17 @@ def notes():
         else: 
             db.session.add(note)
             db.session.commit()
+            return redirect(url_for('notes.notes'))
 
     user_notes=Note.query.filter_by(user_id=current_user.id).all()
     return render_template("notes.html", notes=user_notes)
+
+@notes_bp.route("/delete-note/<int:note_id>", methods=["POST"])
+@login_required
+def delete_note(note_id):
+    note = Note.query.get(note_id)
+    if note and note.user_id == current_user.id:
+        db.session.delete(note)
+        db.session.commit()
+        return jsonify({"status": "success"}), 200
+    return jsonify({"status": "error"}), 403
